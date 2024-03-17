720e6af2d94ef2921454994c76df558ef5889405
rockjoon <rockpago@gmail.com>
2024-03-17 21:48:40 +0900

    add : basic security config

---
+++ src/main/java/com/realworld/auth/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.realworld.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManager -> sessionManager.sessionCreationPolicy(STATELESS))
+                .authorizeHttpRequests(
+                        request -> request.requestMatchers(HttpMethod.POST, "/api/users", "/api/login")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.GET,
+                                        "/api/article/{slug}")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
